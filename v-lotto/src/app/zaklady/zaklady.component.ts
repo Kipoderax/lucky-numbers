723791d19ad7bf6e8f3f05fa8f2b99c8a76928cf
Kipoderax <you@example.com>
2019-03-23 01:17:00 +0100

    init

---
+++ v-lotto/src/app/zaklady/zaklady.component.ts
@@ -0,0 +1,15 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-zaklady',
+  templateUrl: './zaklady.component.html',
+  styleUrls: ['./zaklady.component.css']
+})
+export class ZakladyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
