41f72fb7fdd5e8b2f8d6571268c3cc17d3316232
Kipoderax <detroid90@wp.pl>
2019-07-28 22:48:34 +0200

    test props

--- src/test/java/kipoderax/virtuallotto/VirtualLottoApplicationTests.java
+++ src/test/java/kipoderax/virtuallotto/VirtualLottoApplicationTests.java
@@ -1,9 +1,16 @@
 package kipoderax.virtuallotto;
 
+import kipoderax.virtuallotto.config.AppConfigForJasyptStarter;
+import kipoderax.virtuallotto.config.PropertyServiceForJasyptStarter;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -12,5 +19,20 @@ public class VirtualLottoApplicationTests {
     @Test
     public void contextLoads() {
     }
+
+//    @Test
+//    public void whenDecryptedPasswordNeeded_GetFromService() {
+//        System.setProperty("jasypt.encryptor.password", "password");
+//        PropertyServiceForJasyptStarter service = appCtx.
+//                getBean(PropertyServiceForJasyptStarter.class);
+//
+//        assertEquals("Password@1", service.getProperty());
+//
+//        Environment environment = appCtx.getBean(Environment.class);
+//
+//        assertEquals(
+//                "Password@1",
+//                service.getPasswordUsingEnvironment(environment));
+//    }
 
 }
